70f0db4de38bab2e61f93545ed5dad498d856ff1
Sitthichok Kitpluem <fluke.sitthichok.work@gmail.com>
2017-02-24 13:25:36 +0700

    Add Controller

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index');
+Route::get('/', 'TopicController@index');
+Route::resource('/topic', 'TopicController');
+Route::post('/topic/{id}', 'TopicController@comment');
