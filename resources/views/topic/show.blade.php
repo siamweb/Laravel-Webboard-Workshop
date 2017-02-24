7ab09fe9cdb9721d8137d6a8184045f264b44610
Sitthichok Kitpluem <fluke.sitthichok.work@gmail.com>
2017-02-24 15:33:34 +0700

    Add comment

--- resources/views/topic/show.blade.php
+++ resources/views/topic/show.blade.php
@@ -27,10 +27,40 @@
 
                             <div class="text-right">
                                 By <strong>{{ $comment->user->name }}</strong>
-                                at <strong>{{ $comment->created_at }}</strong>
+                                at <strong>{{ $comment->created_at->diffForHumans() }}</strong>
                             </div>
                         @endforeach
 
+
+                         @if(! Auth::guest())
+    <hr />
+    <div>
+        <form class="form-horizontal" role="form" method="POST" action="{{ action('TopicController@comment', $topic->id) }}">
+            {{ csrf_field() }}
+
+            <div class="form-group{{ $errors->has('body') ? ' has-error' : '' }}">
+                <label for="body" class="col-md-4 control-label">Comment</label>
+
+                <div class="col-md-6">
+                    <textarea id="body" type="body" class="form-control" name="body" required>{{ old('body') }}</textarea>
+                    @if ($errors->has('body'))
+                        <span class="help-block">
+                                            <strong>{{ $errors->first('body') }}</strong>
+                                        </span>
+                    @endif
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-8 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        Post
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endif   
 
                     </div>
                 </div>
